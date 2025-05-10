be7c4593b2954037e9f4146759973fef59048f0a
Dhakshnan <150678283+Dhakshnan@users.noreply.github.com>
2025-05-10 00:47:37 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/navbar"
 import Footer from "./components/footer"
 import Home from "./pages/home"
@@ -10,31 +10,23 @@ import Order from "./pages/Order"
 import FilterData from "./pages/FilterData"
 import ProductDetails from "./pages/ProductDetails"
 
-
-
-
-
-
-
-
 function App() {
-   const [order,setOrder] = useState(null)
- 
+  const [order, setOrder] = useState(null)
 
   return (
-   <BrowserRouter>
-   <Navbar />
-     <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/Shop" element={<Shop />}></Route>
-        <Route path="/Cart" element={<Cart />}></Route>
-        <Route path="/Checkout" element={<Checkout setOrder={setOrder} />}></Route>
-        <Route path="/order-confirmation" element={<Order order={order} />}></Route>
-        <Route path="/filter-data" element={<FilterData />}></Route>
-        <Route path="/product/:id" element={<ProductDetails />}></Route>
-     </Routes>
-   <Footer />
-   </BrowserRouter>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Shop" element={<Shop />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Checkout" element={<Checkout setOrder={setOrder} />} />
+        <Route path="/order-confirmation" element={<Order order={order} />} />
+        <Route path="/filter-data" element={<FilterData />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
