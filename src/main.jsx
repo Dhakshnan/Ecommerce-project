5707de1009f3223f0b91d6a24991cf7f4988ab61
Dhakshnan <150678283+Dhakshnan@users.noreply.github.com>
2025-05-10 00:40:08 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,20 @@ import './index.css'
 import App from './App.jsx'
 import store from './redux/store.jsx'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
+const basename = import.meta.env.BASE_URL;
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
 
-
+   <BrowserRouter basename={basename}>
     <Provider store={store}>
     <App />
     </Provider>
-      
+      </BrowserRouter>
     
  
   </React.StrictMode>,
-)
+);
